00a17e98be3383c3b78270c98f0c595eaf2fef1c
Beau Kinstler <git@beaukinstler.info>
2017-12-22 09:07:02 -0500

    Update to pep8 standards

--- database_setup.py
+++ database_setup.py
@@ -1,37 +1,47 @@
-import sys
-import os
-
+"""
+This is a class to setup the data connections for a
+restaurant menu web application
+"""
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
-Base = declarative_base()
+BASE = declarative_base()
 
 
-class Restaurant(Base):
+class Restaurant(BASE):
+    """
+    Define restaurant class
+    Extends a constant of an instance of declarative_base()
+    """
 
     __tablename__ = 'restaurant'
 
-    name = Column( String(80), nullable = False )
-    id = Column( Integer, primary_key = True )
+    name = Column(String(80), nullable=False)
+    res_id = Column(Integer, primary_key=True)
 
-class MenuItem(Base):
+
+class MenuItem(BASE):
+    """
+    Define restaurant class
+    Extends a constant of an instance of declarative_base()
+    """
 
     __tablename__ = 'menu_item'
 
-    name = Column( String(80), nullable = False )
-    id = Column(Integer, primary_key = True )
+    name = Column(String(80), nullable=False)
+    men_id = Column(Integer, primary_key=True)
 
-    course = Column( String(250) )
-    description = Column( String(250) )
-    price = Column( String(8))
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
 
-    restaurant_id = Column( Integer, ForeignKey('restaurant.id') )
+    restaurant_id = Column(Integer, ForeignKey('restaurant.res_id'))
 
     restaurant = relationship(Restaurant)
 
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+ENGINE = create_engine('sqlite:///restaurantmenu.db')
 
-Base.metadata.create_all(engine)
+BASE.metadata.create_all(ENGINE)
