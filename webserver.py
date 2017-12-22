b2815563bd87be1c4386d3a18ed0ed078550a382
Beau Kinstler <git@beaukinstler.info>
2017-12-22 13:31:37 -0500

    update the webserver code for pep8

--- webserver.py
+++ webserver.py
@@ -6,7 +6,7 @@ import cgi
 
 
 def build_form():
-    
+
     form = ''
     form += '''<form method='POST' enctype='multipart/form-data' action='/hello'>
                 <h2>What would you like me to say?</h2>
@@ -17,8 +17,9 @@ def build_form():
     output += "<h2>What would you like me to say?</h2>"
     output += "<input name='message' type='text'>"
     output += "<input type='submit' value='Submit'> </form>"
-    
+
     return form
+
 
 class webserverHandler(BaseHTTPRequestHandler):
 
@@ -31,7 +32,7 @@ class webserverHandler(BaseHTTPRequestHandler):
             ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
             if ctype == 'multipart/form-data':
                 fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent =  fields.get('message')
+                messagecontent = fields.get('message')
 
             output = ""
             output += "<html><body>"
@@ -44,14 +45,12 @@ class webserverHandler(BaseHTTPRequestHandler):
         except:
             pass
 
-
-
     def do_GET(self):
 
         try:
             if self.path.endswith("/hello"):
                 self.send_response(200)
-                self.send_header('Content-type','text/html')
+                self.send_header('Content-type', 'text/html')
                 self.end_headers()
 
                 output = ""
@@ -63,10 +62,10 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output)
                 print(output)
                 return
-            
+
             if self.path.endswith("/hola"):
                 self.send_response(200)
-                self.send_header('Content-type','text/html')
+                self.send_header('Content-type', 'text/html')
                 self.end_headers()
 
                 output = ""
@@ -82,13 +81,13 @@ class webserverHandler(BaseHTTPRequestHandler):
 
         except:
             self.send_error(400, "File not found: %s" % self.path)
-            
+
 
 def main():
     try:
         port = 8081
-        server = HTTPServer(('',port), webserverHandler)
-        print("Running web server on port %s",port)
+        server = HTTPServer(('', port), webserverHandler)
+        print("Running web server on port %s", port)
         server.serve_forever()
 
     except KeyboardInterrupt:
@@ -98,4 +97,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
