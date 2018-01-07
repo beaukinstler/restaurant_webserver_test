4ef3b4dc69211600caa2536d310333f77c0296b7
Beau Kinstler <git@beaukinstler.info>
2018-01-06 19:35:55 -0500

    Move more of the html the helper function

--- restaurants_views.py
+++ restaurants_views.py
@@ -4,8 +4,12 @@ build html snipits from db objects
 
 
 def restaurant_list(restaurants):
-    output = "<div><ul>"
+    output = ""
+    output += "<html><body>"
+    output += "<h1> Restaurants: </h1>"
+    output += "<div><ul>"
     for restaurant in restaurants:
         output += "<li>" + restaurant.name + "</li>"
     output += "</ul></div>"
+    output += "</body></html>"
     return output
