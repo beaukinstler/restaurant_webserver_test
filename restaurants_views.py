2ecd6bc0c6fc0190f6806656447f0675ccb9fae9
Beau Kinstler <git@beaukinstler.info>
2018-01-13 20:18:23 -0500

    Add code to make it more like a user friendly page
    Based on the requirements for the excercise, making the make list have links
    The core logic driving the chagnes, is the addition of the
    path_parser function, allowing for getting id and action
    form the intent of the path sent to the server.

--- restaurants_views.py
+++ restaurants_views.py
@@ -8,6 +8,9 @@ def restaurant_list(restaurants):
     output += "<h1> Restaurants: </h1>"
     output += "<div><ul>"
     for restaurant in restaurants:
-        output += "<li>" + restaurant.name + " - " + str(restaurant.res_id) + "</li>"
+        output += "<li>" + restaurant.name + " - " + str(restaurant.res_id)
+        output += "- <a href='/restaurant/" + str(restaurant.res_id) + "/edit'>Edit</a> - "
+        output += "<a href='/restaurant/" + str(restaurant.res_id) + "/delete'>Delete</a>"
+        output += "</li>"
     output += "</ul></div>"
     return output
--- webserver.py
+++ webserver.py
@@ -38,42 +38,56 @@ def build_new_restaurant_form():
 
     return form
 
-def build_del_restaurant_form():
+def build_del_restaurant_form(id):
     """
     Build a form for adding a deleting a restaurant from the database
 
-    Args: String - Name of the restaurant TODO: format this like other function code headings
+    Args: String - id of the restaurant to delete
     """
+
+    current_name = db_command.get_restaurant(int(id)).name
 
     form = ''
     form += '''<form method='POST' enctype='multipart/form-data' action='/restaurant/delete'>
-                <h2>Enter the ID number of the restaurant to delete:</h2>
-                <input name="restaurant_id" type="text" >
+                <h2>Hit submit to delete {}</h2>
+                <input type="hidden" name="restaurant_id" type="text" value="'''.format(current_name)
+    form += str(id)            
+    form += '''"><input type="submit" value="Submit"> </form>'''
+
+    return form
+
+def build_update_restaurant_form(id):
+    """
+    Build a form for adding a updating a restaurant from the database
+
+    Args: String - Name of the restaurant TODO: format this like other function code headings
+    """
+    current_name = db_command.get_restaurant(int(id)).name
+    form = ''
+    form += '''<form method='POST' enctype='multipart/form-data' action='/restaurant/update'>
+                <h2>Make your name change:</h2>
+                <input name="restaurant_id" type="hidden" value="'''
+    form += id
+    form += '''"><input name="restaurant_name" type="text" value="'''
+    form += current_name
+    form += '''">
                 <input type="submit" value="Submit"> </form>'''
 
     return form
 
+def path_parser(self):
+    """
+    Purpose: Figure out the purpose of the path
+    """
+
+    primary_path = self.path.partition('/')[2]
+    dir1 = primary_path.partition('/')[0]
+    action_path = primary_path.partition('/')[2]
+    id = action_path.partition('/')[0]
+    action = action_path.partition('/')[2]
+    return dir1,id,action
 
 class webserverHandler(BaseHTTPRequestHandler):
-
-    def do_DELETE(self):
-        print("Made it here! DELETE methods are at least getting called")
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-        print(type(ctype))
-
-        if ctype == 'multipart/form-data':
-            fields = cgi.parse_multipart(self.rfile, pdict)
-            if self.path.endswith("/restaurant/delete"):
-                try:
-                    restaurant_id = fields.get('restaurant_id')
-                    db_command.delete_restaurant(restaurant_id)        
-
-                except:
-                    print("Wasn't able to get the restaurant ID or delete it without error")
-                    pass
 
     def do_POST(self):
         
@@ -111,10 +125,29 @@ class webserverHandler(BaseHTTPRequestHandler):
                         restaurant_id = fields.get('restaurant_id')[0]
                         print(restaurant_id)
                         db_command.delete_restaurant(int(restaurant_id)) 
-                        output += "<p> Restaurant " + restaurant_id + " deleted.</p>"    
+                        output += "<h3> Restaurant " + restaurant_id + " deleted.</h3>"    
 
                     except:
                         print("Wasn't able to get the restaurant ID or delete it without error")
+                        output += "<h3> Wasn't able to get the restaurant ID or delete it without error:"
+                        output += "<br> ID: " + restaurant_id + " not deleted.</h3>"
+                        pass
+
+                elif self.path.endswith("/restaurant/update"):
+                    try:
+                        restaurant_id = fields.get('restaurant_id')[0]
+                        # print(restaurant_id)
+                        restaurant_name = fields.get('restaurant_name')[0]
+                        # print(restaurant_name)
+                        print("Trying to update restaurant id : " + str(restaurant_id) )
+                        db_command.update_restaurant(int(restaurant_id), restaurant_name) 
+                        output += "<h3> Restaurant " + str(restaurant_id) + " updated with name: "
+                        output += str(restaurant_name) + ".</h3>"    
+
+                    except:
+                        print("Wasn't able to get the restaurant ID or update it without error")
+                        output += "<h3> Wasn't able to get the restaurant ID or update it without error:"
+                        output += "<br> ID: " + restaurant_id + " not updated.</h3>"
                         pass
 
                 elif self.path.endswith("/hello"):
@@ -141,21 +174,54 @@ class webserverHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
 
+
+
+
         try:
+            
+            #parse the path to figure out what to do
+            path = path_parser(self)
+            action = path[2]
+            id = path[1]
+            obj = path[0]
+
+            # Send the headers as long as it makes sense ???
+            # if obj != '':
+                # self.send_response(200)
+                # self.send_header('Content-type', 'text/html')
+                # self.end_headers()
+
+            output = ""
+            output += "<html><body>"
+
+            if action == 'edit':
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                # form to edit an item
+                output += build_update_restaurant_form(id)
+
+
+            elif action == 'delete':
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                # form to delete an item
+                output += build_del_restaurant_form(id)
+            
             if self.path.endswith("/hello"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
 
-                output = ""
-                output += "<html><body>"
+            
                 output += "<h1> Hello to you! </h1>"
                 output += build_form()
-                output += "</body></html>"
+                
 
-                self.wfile.write(output)
-                print(output)
-                return
+                # self.wfile.write(output)
+                # print(output)
+                # return
 
             elif self.path.endswith("/hola"):
                 self.send_response(200)
@@ -169,9 +235,9 @@ class webserverHandler(BaseHTTPRequestHandler):
                 output += build_form()
                 output += "</body></html>"
                 output += "</body></html>"
-                self.wfile.write(output)
-                print(output)
-                return
+                # self.wfile.write(output)
+                # print(output)
+                # return
 
             elif self.path.endswith("/restaurants"):
                 self.send_response(200)
@@ -183,9 +249,9 @@ class webserverHandler(BaseHTTPRequestHandler):
                 output += restaurants_views.restaurant_list(db_command.get_all_restaurants())
                 output += "</body></html>"
 
-                self.wfile.write(output)
-                print(output)
-                return        
+                # self.wfile.write(output)
+                # print(output)
+                # return        
             
             elif self.path.endswith("/restaurant/new"):
                 self.send_response(200)
@@ -198,25 +264,44 @@ class webserverHandler(BaseHTTPRequestHandler):
                 output += build_new_restaurant_form()
                 output += "</body></html>"
 
-                self.wfile.write(output)
-                print(output)
-                return
+                # self.wfile.write(output)
+                # print(output)
+                # return
 
             elif self.path.endswith("/restaurant/delete"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
-
+                restaurants = db_command.get_all_restaurants()
                 output = ""
                 output += "<html><body>"
                 output += "<h1>They come and they go! </h1>"
-                output += build_del_restaurant_form()
-                output += restaurants_views.restaurant_list(db_command.get_all_restaurants())
+                output += build_del_restaurant_form(str(restaurants[0].res_id)) # TODO this no longer makes sense the 1 is a bandaid
+                output += restaurants_views.restaurant_list(restaurants)
                 output += "</body></html>"
 
-                self.wfile.write(output)
-                print(output)
-                return
+                # self.wfile.write(output)
+                # print(output)
+                # return
+
+            elif self.path.endswith("/restaurant/update"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                restaurants = db_command.get_all_restaurants()
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Change is fine! </h1>"
+                output += build_update_restaurant_form(str(restaurants[0].res_id)) # TODO this no longer makes sense. the 1 is a bandaid
+                output += restaurants_views.restaurant_list(restaurants)
+                output += "</body></html>"
+
+            # Wrap up the output, and send
+            output += "</body></html>"
+            self.wfile.write(output)
+            print(output)
+            return
+
         except:
             self.send_error(400, "File not found: %s" % self.path)
 
