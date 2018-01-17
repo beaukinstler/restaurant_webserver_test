8a717f1f201480ebed763605aacb8deee94386e7
Beau Kinstler <git@beaukinstler.info>
2018-01-17 15:04:28 -0500

    Udacity quiz version

--- project.py
+++ project.py
@@ -3,7 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import BASE, Restaurant, MenuItem
 
-
 app = Flask(__name__)
 
 engine = create_engine('sqlite:///restaurantmenu.db')
@@ -14,36 +13,39 @@ session = DBSession()
 
 
 @app.route('/')
-@app.route('/menu')
-def Index():
-    '''
-    Main landing page, shows all menu items on first restuarant
-    '''
-    restaurants = session.query(Restaurant)
-    # items = session.query(MenuItem).filter_by(restaurant_id = restaurant.res_id)
-    output = "<ul>"
-    output = "<li> NAME - ID</li>"
-    for res in restaurants:
-        output += "<li><a href='/restaurant/{1}/menu'>{0} - {1}</a></li>".format(res.name,res.res_id)
-    output += "</ul>"
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+        output += i.price
+        output += '</br>'
+        output += i.description
+        output += '</br>'
+        output += '</br>'
     return output
 
-@app.route('/restaurant/<int:res_id>/menu')
-def Menu(res_id):
-    '''
-    Menu for a restaurant
-    '''
-    restaurant = session.query(Restaurant).filter_by(res_id=res_id).first()
-    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.res_id)
-    output = "<h1>Menu for {}</h1>".format(restaurant.name)
-    output += "<ul>"
-    for item in items:
-        output += "<li>{}</li>".format(item.name,)
-    output += "</ul>"
-    return output
+# Task 1: Create route for newMenuItem function here
+
+
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+# Task 2: Create route for editMenuItem function here
+
+
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+# Task 3: Create a route for deleteMenuItem function here
+
+
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
 
 if __name__ == '__main__':
-      app.debug = True
-      app.run( host = '0.0.0.0', port = 5001 )
-
-
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
