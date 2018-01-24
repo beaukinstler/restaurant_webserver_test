ae566d9028ffdebb93efb6e05d8e13c7febff140
Beau Kinstler <git@beaukinstler.info>
2018-01-23 22:30:15 -0500

    Add routes and POST and GET handeling for new pages

--- project.py
+++ project.py
@@ -1,8 +1,9 @@
 from flask import Flask, url_for, render_template, request, redirect, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import BASE, Restaurant, MenuItem
+from database_setup import BASE, Restaurant, MenuItem, Category
 from db_command import *
+from restaurants_views import *
 import pdb
 
 
@@ -14,15 +15,16 @@ BASE.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-
 @app.route('/')
+@app.route('/restaurants')
 def index():
     restaurants = get_all_restaurants()
 
     return render_template('index.html',restaurants=restaurants)
         
 
-@app.route('/restaurants/<int:restaurant_id>/')
+@app.route('/restaurant/<int:restaurant_id>/')
+@app.route('/restaurant/<int:restaurant_id>/menu')
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(res_id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.res_id)
@@ -30,7 +32,34 @@ def restaurantMenu(restaurant_id):
     user = 'admin'
     return render_template('menu.html',restaurant=restaurant,items=items,user=user)
 
-# Task 1: Create route for newMenuItem function here
+@app.route('/restaurant/new', methods=['GET', 'POST'])
+def newRestaurant():
+    if request.method == 'POST':
+        new_id = add_restaurant(request.form['name'],request.form['option'])
+        flash("Restaurant added!")
+        return redirect(url_for('restaurantMenu', restaurant_id=new_id))
+    else:
+        categories = get_all_categories()
+        return render_template('newrestaurant.html',categories=categories)
+
+@app.route('/restaurant/<int:restaurant_id>/edit', methods=['GET', 'POST'])
+def editRestaurant():
+    if request.method == 'POST':
+        new_id = update_restaurant(restaurant_id,request.form['name'],request.form['category_id'])
+        flash("Restaurant updated!")
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return "TODO: form for updating a restaurant"
+
+@app.route('/restaurant/<int:restaurant_id>/delete', methods=['GET', 'POST'])
+def deleteRestaurant(restaurant_id):
+    if request.method == 'POST':
+        delete_restaurant(restaurant_id)
+        flash("Restaurant deleted!")
+        return redirect(url_for('index'))
+    else:
+        restaurant = get_restaurant(restaurant_id)
+        return render_template('deleterestaurant.html',restaurant=restaurant)
 
 @app.route('/restaurant/<int:restaurant_id>/menu/new', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
@@ -38,9 +67,8 @@ def newMenuItem(restaurant_id):
     # restaurant = session.query(Restaurant).filter_by(res_id=restaurant_id).one()
     # items = session.query(MenuItem).filter_by(restaurant_id=restaurant.res_id)
 
-    # return "page to create a new menu item. Task 1 complete!"
     if request.method == 'POST':
-        new_id = add_menu_item(restaurant_id,request.form['name'],request.form['price'])
+        new_id = add_menu_item(restaurant_id,request.form['name'],request.form['description'],request.form['price'])
         flash("New menu item created!")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
@@ -52,7 +80,7 @@ def newMenuItem(restaurant_id):
 def editMenuItem(restaurant_id, menu_id):
     # return "page to edit a menu item. Task 2 complete!"
     if request.method == 'POST':
-        update_menu_item(menu_id,request.form['name'],request.form['price'])
+        update_menu_item(menu_id, request.form['name'], request.form['description'], request.form['price'])
         flash("Item updated!")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
@@ -86,6 +114,51 @@ def getAllMenuItemJson(restaurant_id):
 def getMenuItemJson(restaurant_id, menu_id):
     item = get_menu_item(menu_id)
     return jsonify(MenuItems=item.serialize)
+
+@app.route('/categories', methods=['GET'])
+def getAllCategories():
+    categories = get_all_categories()
+
+    return render_template('categories.html',categories=categories)
+
+@app.route('/category/<int:category_id>', methods=['GET'])
+def category(category_id):
+    category = get_category(category_id)
+
+    return render_template('category.html',category=category)
+
+@app.route('/category/new', methods=['GET', 'POST'])
+def newCategory():
+    if request.method == 'POST':
+        new_id = add_category(request.form['category'])
+        flash("Category added!")
+        return redirect(url_for('getAllCategories'))
+    else:
+        return render_template('newcategory.html')
+
+@app.route('/category/<int:category_id>/edit', methods=['GET', 'POST'])
+def editCategory(category_id):
+    if request.method == 'POST':
+        update_category(category_id, request.form['category_name'])
+        flash("Category updated!")
+        return redirect(url_for('getAllCategories'))
+    else:
+        category = get_category(category_id)
+        return render_template('editcategory.html',category=category)
+
+@app.route('/category/<int:category_id>/delete', methods=['GET','POST'])
+def deleteCategory(category_id):
+
+    if request.method == 'POST':
+        if request.form['delete'] == 'Delete':
+            delete_category(category_id)
+            flash("Category deleted!")
+        elif request.form['delete'] == 'Cancel':
+            pass
+        return redirect(url_for('getAllCategories'))
+    else:
+        category = get_category(category_id)
+        return render_template('deletecategory.html',category=category)
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
